7ad124b87b904472616001601a0a7d7a02c6ec40
Suvendu <suvendu.chok@gmail.com>
2022-09-01 09:48:32 +0530

    Stage 2

--- Django-Form/Form/service/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from service.models import Service
-
-class ServiceAdmin(admin.ModelAdmin):
-    list_display=('name','roll','reg','email')
-
-
-admin.site.register(Service,ServiceAdmin)
--- Django-Form/Form/service/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Service(models.Model):
-    name=models.CharField(max_length=30)
-    email=models.CharField(max_length=20)
-    roll=models.CharField(max_length=30)
-    reg=models.CharField(max_length=30)
